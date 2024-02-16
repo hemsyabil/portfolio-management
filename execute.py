dd38b20a25099f6a509bc50496751763b143fec9
Hem Raj Regmi <sangamsyabil@gmail.com>
2024-02-16 15:35:32 -0500

    initial structure is done

---
+++ execute.py
@@ -0,0 +1,8 @@
+# This is the main executable file for this web application
+
+def run():
+    print("Welcome to Anila's Portfolio Management Application 2024")
+
+
+if __name__ == '__main__':
+    run()
